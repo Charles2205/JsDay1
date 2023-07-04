5807bf1f0afa52143edb1f6fb82a524bce522e00
Charles2205 <cewudzie0@gmail.com>
2023-07-04 12:28:16 +0000

    Started Js & HTML

---
+++ main.js
@@ -0,0 +1,11 @@
+// Accessing tags in javascript
+                        // getElementsByTagName
+// const heading = document.body.getElementsByTagName('h1')[0]
+// heading.innerHTML='Hi Charles This is your first javascript instruction'
+
+//                      getElementsById
+// const headingID= document.getElementById('hello');
+// headingID.innerHTML='THis is a second javascript instruction'
+
+const heading = document.querySelector('h1');
+heading.innerText='Charles';
