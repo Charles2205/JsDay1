48bdd1ec94e8ead7c4be2d37a03cb8b2fb3c266c
Charles2205 <cewudzie0@gmail.com>
2023-07-05 13:11:07 +0000

    Created an ECG Simple site

--- public/js/main.js
+++ public/js/main.js
@@ -35,4 +35,18 @@ Double click
 
 
 const btnClick = document.querySelector('#btn-click');
-console.log(btnClick);
+btnClick.addEventListener('click', () => {
+//    accessing the body
+    let body = document.body.style.backgroundImage;
+    // splitting the directory to get 'imageName'
+    const currentImg= body.split("\"")[1].split("/")[3];
+
+    // Checking the current image or stage
+    if(currentImg==='lighton.png'){
+        document.body.style.backgroundImage = 'url("./public/imgs/turnoff.jpeg")'
+        btnClick.innerHTML = 'Turn on';
+    }else{
+        document.body.style.backgroundImage = 'url("./public/imgs/lighton.png")'
+        btnClick.innerHTML = 'Turn off';
+    }
+});
