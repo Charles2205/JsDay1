9d4ece61f0ddcf063387b2299da5c15353c6020b
Charles Ewudzi <cewudzie0@gmail.com>
2023-07-09 10:18:26 +0000

    Add files via upload

--- Countdown_app/public/js/main.js
+++ Countdown_app/public/js/main.js
@@ -1,6 +1,19 @@
 function counter() {
     for (i=1;i<=60;i++){
-        console.log(i);
+        ;
     }    
 }
 counter()
+// Targeting elements
+const timer=document.querySelector('#timer')
+const startBtn=document.querySelector('#start-btn')
+const stopBtn=document.querySelector('#stop-btn')
+const resetBtn =document.querySelector('#reset-btn')
+
+// Event handlers
+startBtn.addEventListener('click',start)
+
+// functions for handle events
+function start(){
+    alert('hello World')
+}
